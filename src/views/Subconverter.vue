22e8b7410999d5b28e7c7dddbf66371448d1f049
fansa92 <fansa91@outlook.com>
2025-07-02 11:17:36 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,6 +338,8 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
+          "my【vless reality+hy1+hy2】": "https://subapi.fansa.hidns.vip",
+          "大象": "https://sub88.dxdyzh.tk",
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
@@ -347,6 +349,8 @@ export default {
           "sub-web作者提供": "https://api.wcc.best",
         },
         backendOptions: [
+          { value: "https://subapi.fansa.hidns.vip" },
+          { value: "https://sub88.dxdyzh.tk" },
           { value: "https://subapi.cmliussss.net" },
           { value: "https://subapi.fxxk.dedyn.io" },
           { value: "https://url.v1.mk" },
